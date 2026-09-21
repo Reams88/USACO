c17e77551da04a5546f6ecf4c37a7df7f8f7fdda
Kotei Timofii <school59timofii@gmail.com>
2026-09-21 12:17:05 +0300

    something again

---
+++ bronze/introductionToSorting/hard/permutator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+using ull = unsigned long long;
+
+int main() {
+  //freopen("blist.in", "r", stdin);
+  //freopen("blist.out", "w", stdout);
+
+
+  return 0;
+}
